28702c3326c53f896041aa891caeff039bc60623
FOSFO7 <75679629+FOSFO7@users.noreply.github.com>
2025-02-06 17:10:20 +0100

    declaracio constructor objecte productes.

---
+++ src/main/java/Producte.java
@@ -0,0 +1,12 @@
+public class Producte {
+    private String nombre;
+    private String origen;
+    private double precio;
+    private int stock;
+    public Producte(String nombre, String origen, double precio, int stock){
+        this.nombre = nombre;
+        this.origen = origen;
+        this.precio = precio;
+        this.stock = stock;
+    }
+}
