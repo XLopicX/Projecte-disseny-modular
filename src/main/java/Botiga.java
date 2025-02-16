aa374e5fb6869aab880ee811f7e9847bfb0027be
FOSFO7 <75679629+FOSFO7@users.noreply.github.com>
2025-02-16 22:56:47 +0100

    Agregat producte a botiga

--- src/main/java/Botiga.java
+++ src/main/java/Botiga.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+
 public class Botiga {
     private String nombreTienda;
+    private ArrayList<Producte> Productes = new ArrayList<>();
     public Botiga(String nombreTienda){
         this.nombreTienda = nombreTienda;
+    }
+
+    public void addProducto(Producte producte){
+        Productes.add(producte);
     }
 
 }
