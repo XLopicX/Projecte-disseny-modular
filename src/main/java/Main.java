f42165d9224bf1e05bc78944cf17c372ca2a5bd0
Ferripro <gitea.percolate713@passmail.net>
2025-02-18 18:56:21 +0100

    Agregat exemple de funcionament basic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Botiga greenClick = new Botiga("Green & Click");
 
+        Producte mariaJuana = new Producte("Maria Juana", "Mexico", 1.70);
+        Producte hierbaBosque = new Producte("Hierba de los bosque", "Andorra", 4.20);
+
+        greenClick.addProducto(mariaJuana, 3);
+        greenClick.addProducto(hierbaBosque, 4);
+
+        greenClick.buscarProducto("ma");
     }
 }
