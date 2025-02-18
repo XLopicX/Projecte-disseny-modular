6cb77c18c18d844e193eacb02ef4e61411ab70d1
Ferripro <gitea.percolate713@passmail.net>
2025-02-18 18:55:58 +0100

    Agregat metode buscarProducto i agregat atribut a stock a addProducto.

--- src/main/java/Botiga.java
+++ src/main/java/Botiga.java
@@ -3,12 +3,31 @@ import java.util.ArrayList;
 public class Botiga {
     private String nombreTienda;
     private ArrayList<Producte> Productes = new ArrayList<>();
+    private ArrayList<Integer> Stock = new ArrayList<>();
+
     public Botiga(String nombreTienda){
         this.nombreTienda = nombreTienda;
     }
 
-    public void addProducto(Producte producte){
+    public void addProducto(Producte producte, Integer stock){
         Productes.add(producte);
+        Stock.add(stock);
     }
+
+    public int buscarProducto(String input) {
+        input = input.toLowerCase();
+
+        int trobats = 0;
+
+        for (Producte prod : Productes) {
+            if (prod.getNombre().toLowerCase().contains(input)) {
+                trobats++;
+                System.out.println(String.format("[%d] %s", trobats, prod.getNombre()));
+            }
+        }
+
+        return trobats;
+    }
+
 
 }
